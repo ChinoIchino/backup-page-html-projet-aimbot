6ee6ff03778bd9a897a88e9afb09c7fb411b8889
KamilBlaszkow <97180792+KamilBlaszkow@users.noreply.github.com>
2022-01-23 13:59:48 +0100

    backup

---
+++ html page/javascript_pageprincipal.js
@@ -0,0 +1,14 @@
+
+
+
+
+/* test pour lire suite 
+var lire_suite = domument.getElementById('lire_suite');
+var text1 = document.createElement('text1');
+var texte = document.createTextNode('test numero 2 test');
+
+lire_suite.onclick = function() {
+	lire_suite.parentNode.removeChild(lire_suite);
+	document.body.appendChild(text1)
+	text1.appendChild(texte);
+}*/
